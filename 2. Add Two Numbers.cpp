525756b981b330df699f1fdfb2c378f0636ebb5b
JIAHE LIU <32347702+liujiahe971024@users.noreply.github.com>
2019-09-26 17:32:08 -0700

    Done

--- 2. Add Two Numbers.cpp
+++ 2. Add Two Numbers.cpp
@@ -1,4 +1,8 @@
-/**
+/*
+Jiahe Liu
+liujiahe971024@gmail.com
+09/21/2019
+
 2. Add Two Numbers
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
@@ -9,7 +13,7 @@ Example:
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
-
+------------------------------------------------------
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
